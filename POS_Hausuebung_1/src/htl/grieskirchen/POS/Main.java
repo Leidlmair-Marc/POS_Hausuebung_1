9bc0c79ef1760ae6c033056cb4b83d52b4391b48
Marc <leidlmairM21047@sus.htl-grieskirchen.at>
2024-09-17 12:23:41 +0200

    typo created

--- POS_Hausuebung_1/src/htl/grieskirchen/POS/Main.java
+++ POS_Hausuebung_1/src/htl/grieskirchen/POS/Main.java
@@ -3,6 +3,6 @@ package htl.grieskirchen.POS;
 public class Main {
 
     public static void main(String[] args) {
-        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(50);
+        EratosthenesPrimeSieve eratosthenesPrimeSievee = new EratosthenesPrimeSieve(50);
     }
 }
